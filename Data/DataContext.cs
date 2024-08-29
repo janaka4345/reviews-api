4d774f0ed6d9b5c7be04103a9ae8fb29b51d27f8
Janaka dev <janakchamantha12@gmail.com>
2024-08-29 21:34:11 +0530

    feat:data context for the database added

---
+++ Data/DataContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using reviews_api.Models;
+
+namespace reviews_api.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+           
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<ItemCategory> ItemCategories  { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Reviewer> Reviewers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ItemCategory>()
+                .HasKey(ic => new { ic.ItemId, ic.CategoryId });
+            modelBuilder.Entity<ItemCategory>()
+                .HasOne(i => i.Item)
+                .WithMany(ic => ic.ItemCategories)
+                .HasForeignKey(ic => ic.ItemId);
+            modelBuilder.Entity<ItemCategory>()
+                .HasOne(c => c.Category)
+                .WithMany(ic => ic.ItemCategories)
+                .HasForeignKey(ic => ic.CategoryId);
+        }
+    }
+}
